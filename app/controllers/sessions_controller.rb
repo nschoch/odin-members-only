421950c755524e3a82722752bb13a69645bcfb1f
Nick Schoch <nschoch@gmail.com>
2019-07-24 16:37:25 -0700

    add remember me button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       flash[:success] = "Log in success!"
       log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       remember @user
       redirect_to @user
     else
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:example)
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+  end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+  end
+end
